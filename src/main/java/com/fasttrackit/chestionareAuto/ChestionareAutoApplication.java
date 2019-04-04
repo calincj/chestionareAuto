94df906658b6fe74f68a78973ca5b1413ff326e3
calincj <acalin.andrei83@gmail.com>
2019-04-04 12:36:18 +0300

    Initial commit

---
+++ src/main/java/com/fasttrackit/chestionareAuto/ChestionareAutoApplication.java
@@ -0,0 +1,13 @@
+package com.fasttrackit.chestionareAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChestionareAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChestionareAutoApplication.class, args);
+	}
+
+}
